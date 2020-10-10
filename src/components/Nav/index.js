7140d6ad7821a75b0ff9aa6c5d5032a7e5c33d1c
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-10 13:15:28 -0700

    created about header and nav components - bare minimum

---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Nav(props) {
+  const {
+    pages = [],
+    setCurrentPage,
+    currentPage,
+  } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentPage.name);
+  }, [currentPage]);
+
+  return (    
+      <nav>
+      </nav>
+  );
+}
+
+export default Nav;
+
