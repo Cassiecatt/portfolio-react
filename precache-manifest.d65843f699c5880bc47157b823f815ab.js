12d1b67cab4ab4a837dde241bcefa09e8971d8a9
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-11 16:17:44 -0700

    Updates

---
+++ precache-manifest.d65843f699c5880bc47157b823f815ab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0501c1823c445fe234a85934aef6bf",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "679399bedc5cd716bbb3",
+    "url": "/portfolio-react/static/css/main.c78ab968.chunk.css"
+  },
+  {
+    "revision": "30d777425cc08c5a6282",
+    "url": "/portfolio-react/static/js/2.d49d31db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.d49d31db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679399bedc5cd716bbb3",
+    "url": "/portfolio-react/static/js/main.a1bebf1e.chunk.js"
+  },
+  {
+    "revision": "2f26d0aed242c262185b",
+    "url": "/portfolio-react/static/js/runtime-main.a4de92d3.js"
+  },
+  {
+    "revision": "e3cc67e0d204a5bf08ea89aa73e5b124",
+    "url": "/portfolio-react/static/media/budget-tracker.e3cc67e0.jpg"
+  },
+  {
+    "revision": "715a82d469ffcfeb7b6fb340e3468283",
+    "url": "/portfolio-react/static/media/job-listing.715a82d4.jpg"
+  },
+  {
+    "revision": "1b86304b83e68ad8bc544ccb3241376a",
+    "url": "/portfolio-react/static/media/password-generator.1b86304b.jpg"
+  },
+  {
+    "revision": "2f727a0ba52bda483878f476f84b2f1d",
+    "url": "/portfolio-react/static/media/portfolio-pic.2f727a0b.jpg"
+  },
+  {
+    "revision": "934bc6f01ee0cd0df130250e9c2c471b",
+    "url": "/portfolio-react/static/media/run-buddy.934bc6f0.jpg"
+  },
+  {
+    "revision": "89d16c5e17225c8b5382997e33e31e45",
+    "url": "/portfolio-react/static/media/social-network.89d16c5e.jpg"
+  }
+]);
